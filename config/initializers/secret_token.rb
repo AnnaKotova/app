34aeed2c51a7352b6c1b97b29b9a563b372df453
AnnaKotova <anuta.kotova@gmail.com>
2013-10-17 14:40:06 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyApp::Application.config.secret_token = '21f1de333c9688795ce82c9fd213fb61723e6cad03186a15a589d5c14266288246f82ef0456bea15405259fea78a81fdc076b1fd68b71df79e48e98ef0745ec7'
---
+++ test/unit/helpers/ussues_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class UssuesHelperTest < ActionView::TestCase
+end
